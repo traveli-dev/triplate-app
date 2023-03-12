73cf7aaf789234730c884fc733a3ced998b4a28d
Daiki Maekawa <daaikii@outlook.jp>
2023-03-13 03:49:48 +0900

    修正

--- front/firebase/scripts/initFirebase.sh
+++ front/firebase/scripts/initFirebase.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+source ./.env
+
 FIREBASE_CONFIG=$(cat << EOS
 {
   "projects": {
-    "default": ${NEXT_PUBLIC_FIREBASE_PROJECT_ID}
+    "default": "${NEXT_PUBLIC_FIREBASE_PROJECT_ID}"
   }
 }
 EOS
--- front/next.config.js
+++ front/next.config.js
@@ -1,13 +1,13 @@
 const path = require('path')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withPWA({
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: true,
   sassOptions: {
     includePaths: [path.join(__dirname, './src/styles')]
   },
-})
+}
 
 module.exports = nextConfig
