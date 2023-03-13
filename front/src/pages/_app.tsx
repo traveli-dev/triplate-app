c453d9ae70c08e3a67e42ebd8507e87ff365e8ff
Daiki Maekawa <daaikii@outlook.jp>
2023-03-14 01:29:29 +0900

    add firebase settings

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import 'normalize.css'
 import '@/styles/global.scss'
+import '@/lib/firebase'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
