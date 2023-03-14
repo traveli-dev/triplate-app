db93728d8aca04dc247af3687273de464d24fabc
whooolemilk <wholemilk25@gmail.com>
2023-03-14 20:33:05 +0900

    storybook修正

--- front/src/stories/components/Navigations/NavigationBottom.stories.tsx
+++ front/src/stories/components/Navigations/NavigationBottom.stories.tsx
@@ -2,10 +2,7 @@ import { StoryObj, ComponentMeta } from '@storybook/react'
 import { NavigationBottom } from '@/components/Navigations'
 
 export default {
-  component: NavigationBottom,
-  render: () => {
-    return <NavigationBottom />
-  }
+  component: NavigationBottom
 } as ComponentMeta<typeof NavigationBottom>
 
 export const Default: StoryObj = {
