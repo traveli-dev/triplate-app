4842f60e91b21bbab8f828b58804ca67911fdeca
eve00 <64850924+eve00@users.noreply.github.com>
2023-03-14 18:27:15 +0900

    ButtonFillを実装

---
+++ front/src/components/Buttons/ButtonFill.tsx
@@ -0,0 +1,15 @@
+import React, { ButtonHTMLAttributes } from 'react'
+import styles from '@/styles/components/Buttons/ButtonFill.module.scss'
+
+type ButtonFillProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: React.ReactNode
+  //TODO: icon
+}
+
+export const ButtonFill = ({ children,...props }: ButtonFillProps) => {
+  return (
+    <button className={styles.button} {...props}>
+      {children}
+    </button>
+  )
+}
