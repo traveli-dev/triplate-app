4809b4ac383abb3dc35246a6205b86978ec5110e
Daiki Maekawa <daaikii@outlook.jp>
2023-03-15 00:48:56 +0900

    utils追加

--- front/firebase/scripts/seeder.ts
+++ front/firebase/scripts/seeder.ts
@@ -1,43 +1,6 @@
-const firebase = require('firebase-admin')
-const {
-  getFirestore,
-  connectFirestoreEmulator
-} = require('firebase-admin/firestore')
-const serviceAcountKey = require('../cert/service-account-key.json')
+import admin from 'firebase-admin'
+import faker from '@faker-js/faker'
+import { readJson } from '@/utils/readJson'
 
-type Aiueo = {
-  name: string
-  age: number
-  id: string
-}
+const serviceAccountKey = readJson('cert/serviceAccountKey.json')
 
-(async () => {
-  try {
-    firebase.initializeApp({
-      credential: firebase.credential.cert({
-        projectId: serviceAcountKey.project_id,
-        clientEmail: serviceAcountKey.client_email,
-        privateKey: serviceAcountKey.private_key
-      })
-    })
-
-    const db = getFirestore()
-    connectFirestoreEmulator(db, 'localhost', 8080)
-
-    const querySnapshot = (await db.collection('users').get()) as Aiueo[]
-    const batch = db.batch()
-    querySnapshot.forEach((postDoc) => {
-      batch.set(
-        db.collection('stamps').doc(postDoc.id),
-        {
-          aaa: 1
-        },
-        { merge: true }
-      )
-    })
-
-    await batch.commit()
-  } catch (err) {
-    console.log(err)
-  }
-})()
