a61c0048c81c8d76eb7805051d400293be922788
ibuki <64850924+eve00@users.noreply.github.com>
2023-03-16 20:13:18 +0900

    TabDaysコンポーネントを実装

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,9 +1,12 @@
+import { TabDays } from '@/stories/components/Tabs/TabDays'
+
 const Index = () => {
   return (
     <>
       <h1>HelloWorld</h1>
       <h2>ホーム</h2>
       <p>abcdeABCDEあいうえお12345</p>
+      <TabDays />
     </>
   )
 }
--- front/src/stories/components/Tabs/TabDays.tsx
+++ front/src/stories/components/Tabs/TabDays.tsx
@@ -1,36 +1,30 @@
-import React from "react"
+import React from 'react'
 import styles from '@/styles/components/Tabs/TabDays.module.scss'
-type TabDaysProps = {
+type TabDaysProps = {}
 
-}
-
-
-export const TabDays = ({ }: TabDaysProps) => {
-
-    return (
-        <div className={styles.tab_days}>
-            <Tabs />
-        </div>
-    )
+export const TabDays = ({}: TabDaysProps) => {
+  return (
+    <div>
+      <Tabs />
+    </div>
+  )
 }
 
 export const Tabs = () => {
-    return (
-        <div className={styles.tabs}>
-            <label>
-                <input className={styles.tab} name={"tab"} type="radio" defaultChecked/>
-                    <em>MENU1</em><span>TEXT1</span>
-            </label>
-            <label>
-                <input className={styles.tab} name={"tab"} type="radio"/>
-                    <em>MENU2</em><span>TEXT2</span>
-            </label>
-            <label>
-                <input className={styles.tab} name={"tab"} type="radio"/>
-                    <em>MENU3</em><span>TEXT3</span>
-            </label>
-        </div>
-    )
+  return (
+    <div className={styles.tabs}>
+      <label>
+        <input name={'tab'} type="radio" defaultChecked />
+        <em>１日目</em>
+      </label>
+      <label>
+        <input name={'tab'} type="radio" />
+        <em>２日目</em>
+      </label>
+      <label>
+        <input name={'tab'} type="radio" />
+        <em>３日目</em>
+      </label>
+    </div>
+  )
 }
-
-
