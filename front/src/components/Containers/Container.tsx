03df96632c16d4a9f2f14df2dbcf778694cd4012
whooolemilk <wholemilk25@gmail.com>
2023-03-16 19:33:22 +0900

    Containerの修正

--- front/src/components/Containers/Container.tsx
+++ front/src/components/Containers/Container.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 import styles from '@/styles/components/Containers/Container.module.scss'
 
 type ContainerProps = {
+  bgColor: 'white' | 'blue' // background-colorを切り替える
+  isFull?: boolean // ページ全体まで高さを伸ばす（設定しなければコンテンツの量で高さが決まる）
   children: React.ReactNode
 }
 
-export const Container = ({ children }: ContainerProps) => {
-  return <div className={styles.container}>{children}</div>
+export const Container = ({ bgColor, isFull, children }: ContainerProps) => {
+  return (
+    <div
+      className={`${styles.container} ${
+        bgColor === 'white' ? styles.bg_white : styles.bg_blue
+      }
+      ${isFull && styles.min_height}`}
+    >
+      {children}
+    </div>
+  )
 }
