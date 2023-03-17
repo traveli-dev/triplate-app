6741a03f76dba2d1480dbf0ef83701d9d17f3790
eve00 <64850924+eve00@users.noreply.github.com>
2023-03-17 18:41:41 +0900

    修正

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,12 +1,9 @@
-import { TabDays } from '@/stories/components/Tabs/TabDays'
-
 const Index = () => {
   return (
     <>
       <h1>HelloWorld</h1>
       <h2>ホーム</h2>
       <p>abcdeABCDEあいうえお12345</p>
-      <TabDays />
     </>
   )
 }
