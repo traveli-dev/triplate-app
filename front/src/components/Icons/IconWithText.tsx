b1c3dc48bf7a105d44cef96e29cdf783f50dc418
whooolemilk <wholemilk25@gmail.com>
2023-03-17 21:23:07 +0900

    IconWithText修正

--- front/src/components/Icons/IconWithText.tsx
+++ front/src/components/Icons/IconWithText.tsx
@@ -27,37 +27,37 @@ export const IconWithText = ({ iconType }: IconWithiconProps) => {
   return (
     <Link href="/" className={styles.wrapper}>
       {iconType === 'qr' && (
-        <>
+        <div>
           <HiOutlineQrcode size={24} />
           <p>QRコード</p>
-        </>
+        </div>
       )}
       {iconType === 'bell' && (
-        <>
+        <div>
           <HiOutlineBell size={24} />
           <p>通知</p>
-        </>
+        </div>
       )}
       {iconType === 'setting' && (
-        <>
+        <div>
           <HiOutlineCog size={24} />
           <p>設定</p>
-        </>
+        </div>
       )}
       {iconType === 'help' && (
-        <>
+        <div>
           <HiOutlineQuestionMarkCircle size={24} />
           <p>ヘルプ</p>
-        </>
+        </div>
       )}
       {iconType === 'mail' && (
-        <>
+        <div>
           <HiOutlineMail size={24} />
           <p>お問い合わせ</p>
-        </>
+        </div>
       )}
       {iconType === 'privacy' && (
-        <>
+        <div>
           <HiOutlineShieldCheck size={24} />
           <div className={styles.wrapper__lines}>
             <p>
@@ -66,10 +66,10 @@ export const IconWithText = ({ iconType }: IconWithiconProps) => {
               ポリシー
             </p>
           </div>
-        </>
+        </div>
       )}
       {iconType === 'twitter' && (
-        <>
+        <div>
           <FiTwitter size={24} />
           <div className={styles.wrapper__lines}>
             <p>
@@ -78,13 +78,13 @@ export const IconWithText = ({ iconType }: IconWithiconProps) => {
               Twitter
             </p>
           </div>
-        </>
+        </div>
       )}
       {iconType === 'hp' && (
-        <>
+        <div>
           <HiOutlineGlobeAlt size={24} />
           <p>HP</p>
-        </>
+        </div>
       )}
     </Link>
   )
