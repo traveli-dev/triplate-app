710d8750190d7a50ef1dfb0dbcbf0b2fd5f15cfd
eve00 <64850924+eve00@users.noreply.github.com>
2023-03-18 16:26:19 +0900

    stories

---
+++ front/src/stories/components/Buttons/ButtonIconRound.stories.tsx
@@ -0,0 +1,8 @@
+import { ButtonIconRound } from '@/components/Buttons'
+import { StoryObj, ComponentMeta } from '@storybook/react'
+
+export default {
+  component: ButtonIconRound
+} as ComponentMeta<typeof ButtonIconRound>
+
+export const Default: StoryObj = {}
