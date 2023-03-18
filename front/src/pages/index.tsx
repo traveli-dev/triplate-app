d9788a53e1508364f21267a7ce9857e3250abc78
whooolemilk <wholemilk25@gmail.com>
2023-03-18 17:49:49 +0900

    Containerの修正

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,12 +1,15 @@
+import { Container } from '@/components/Containers'
 import { Header } from '@/components/Headers'
+import { NavigationBottom } from '@/components/Navigations'
 
 const Index = () => {
   return (
     <>
       <Header href="/" title="GoogleMapから追加" />
-      <h1>HelloWorld</h1>
-      <h2>ホーム</h2>
-      <p>abcdeABCDEあいうえお12345</p>
+      <Container bgColor="blue" isFull>
+        <div>HelloWorld</div>
+      </Container>
+      <NavigationBottom />
     </>
   )
 }
