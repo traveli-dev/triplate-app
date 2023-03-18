6e88ca6ccff7dc06117283222df65a4f725ab06f
whooolemilk <wholemilk25@gmail.com>
2023-03-18 19:23:08 +0900

    修正

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -6,7 +6,7 @@ import '@/styles/global.scss'
 import '@/lib/firebase'
 import { Layout } from '@/components/Layouts'
 import { DefaultSeo } from 'next-seo'
-import SEO from '@/config/next-seo.config'
+import { SEO } from '@/config/next-seo.config'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
