59525151c9e18000f216fd174e2b463c08413452
whooolemilk <wholemilk25@gmail.com>
2023-03-18 19:20:23 +0900

    修正

--- front/src/config/next-seo.config.ts
+++ front/src/config/next-seo.config.ts
@@ -1,19 +1,20 @@
-const SEO = {
+import { DefaultSeoProps } from 'next-seo'
+
+export const SEO: DefaultSeoProps = {
   titleTemplate: '%s | traveli - URLを保存する旅程管理アプリ',
   defaultTitle: 'traveli - URLを保存する旅程管理アプリ',
   description:
     'traveliは旅先で確認したい情報のURLを1つにまとめて友達や家族に共有できるwebアプリです',
-
   openGraph: {
     type: 'website',
     title: 'traveli - URLを保存する旅程管理アプリ',
     description:
       'traveliは旅先で確認したい情報のURLを1つにまとめて友達や家族に共有できるwebアプリです',
     site_name: 'traveli - URLを保存する旅程管理アプリ',
-    url: 'https://traveli-web.vercel.app/',
+    url: 'https://traveli.app/',
     images: [
       {
-        url: 'https://traveli-web.vercel.app/images/ogp.png',
+        url: 'https://traveli.app/images/ogp.png',
         width: 1200,
         height: 630,
         alt: '',
@@ -27,5 +28,3 @@ const SEO = {
     cardType: 'summary_large_image'
   }
 }
-
-export default SEO
