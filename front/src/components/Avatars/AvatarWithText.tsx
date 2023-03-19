034f3e7b2ca5aff8d16923eccf1cf272980de70c
whooolemilk <wholemilk25@gmail.com>
2023-03-19 17:14:19 +0900

    AvatarWithTextコンポーネントの作成

---
+++ front/src/components/Avatars/AvatarWithText.tsx
@@ -0,0 +1,16 @@
+import { Avatar } from '@/components/Avatars/Avatar'
+import styles from '@/styles/components/Avatars/AvatarWithText.module.scss'
+
+type AvatarWithTextProps = {
+  url: string
+  name: string
+}
+
+export const AvatarWithText = ({ url, name }: AvatarWithTextProps) => {
+  return (
+    <div className={styles.wrapper}>
+      <Avatar url={url} size={'sm'} />
+      <p>{name}</p>
+    </div>
+  )
+}
