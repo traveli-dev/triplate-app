916ee022747ddea952aa8f0cab1dc20d118ccb3f
Daiki Maekawa <daaikii@outlook.jp>
2023-03-21 13:00:26 +0900

    hammerjs インストール

--- front/src/hooks/modals/index.ts
+++ front/src/hooks/modals/index.ts
@@ -1,3 +1,4 @@
 export { useModal } from '@/hooks/modals/useModal'
 export { useAriaHidden } from '@/hooks/modals/useAriaHidden'
 export { useFocusTrap } from '@/hooks/modals/useFocusTrap'
+export { useSwipe } from '@/hooks/modals/useSwipe'
