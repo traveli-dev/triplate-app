9a2928fd350d0e84433d3fa66fcc62ba94402823
Daiki Maekawa <daaikii@outlook.jp>
2023-04-04 13:38:49 +0900

    css propsに書き換え

--- front/src/components/Layouts/Layout.tsx
+++ front/src/components/Layouts/Layout.tsx
@@ -1,14 +1,14 @@
-import styled from '@emotion/styled'
+import { css } from '@emotion/react'
 import { theme } from '@/styles/theme'
 
 type LayoutProps = {
   children: React.ReactNode
 }
 export const Layout = ({ children }: LayoutProps) => {
-  return <Main>{children}</Main>
+  return <main css={LayoutStyle}>{children}</main>
 }
 
-const Main = styled.main`
+const LayoutStyle = css`
   max-width: ${theme.breakpoint.md};
   min-width: ${theme.breakpoint.sm};
   min-height: 100vh;
