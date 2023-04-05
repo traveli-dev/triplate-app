33716481984cc15d3bf14051b349e0f4ea42d773
Daiki Maekawa <daaikii@outlook.jp>
2023-04-06 03:15:17 +0900

    不要記述消す

--- front/.storybook/main.ts
+++ front/.storybook/main.ts
@@ -1,7 +1,4 @@
 import type { StorybookConfig } from '@storybook/nextjs'
-import path from 'path'
-
-const toPath = (arg: string) => path.join(process.cwd(), arg)
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)', ],
@@ -18,20 +15,7 @@ const config: StorybookConfig = {
     autodocs: 'tag',
     defaultName: 'Docs',
   },
-  staticDirs: ['../public'],
-  webpackFinal: async (config) => {
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve?.alias,
-          '@emotion/core': toPath('node_modules/@emotion/react'),
-          '@': path.resolve(__dirname, '../src')
-        }
-      }
-    }
-  }
+  staticDirs: ['../public']
 }
 
 export default config
