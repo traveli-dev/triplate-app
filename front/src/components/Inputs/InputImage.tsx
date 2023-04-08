8deec7ecc39c6fd1fc1a6d07d7e31a3e8c488ab3
whooolemilk <wholemilk25@gmail.com>
2023-04-09 03:47:17 +0900

    InputImageにプレビュー機能を追加

--- front/src/components/Inputs/InputImage.tsx
+++ front/src/components/Inputs/InputImage.tsx
@@ -1,16 +1,47 @@
-import styles from '@/styles/components/Inputs/InputImage.module.scss'
+import { InputHTMLAttributes, useState } from 'react'
 import Image from 'next/image'
-import { InputHTMLAttributes } from 'react'
+import { HiOutlineUpload } from 'react-icons/hi'
+import { useInputImage } from '@/hooks/inputs'
+import { styles } from '@/styles/components/Inputs/InputImage.styles'
 
-type InputImageProps = InputHTMLAttributes<HTMLDivElement> & {
-  src: string
+type InputImageProps = InputHTMLAttributes<HTMLInputElement> & {
   alt: string
 }
 
-export const InputImage = ({ src, alt }: InputImageProps) => {
+export const InputImage = ({ alt }: InputImageProps) => {
+  const [image, setImage] = useState<File | null>()
+  const { inputRef, onClickImage } = useInputImage()
+
   return (
-    <div className={styles.image_wrapper}>
-      <Image className={styles.image} src={src} alt={alt} fill />
-    </div>
+    <>
+      <input
+        accept="image/*"
+        css={styles.hidden}
+        ref={inputRef}
+        type="file"
+        onChange={(e) => {
+          if (e.target.files !== null) {
+            const file = e.target.files[0]
+            setImage(file)
+          }
+        }}
+      />
+      {image ? (
+        <div css={styles.previewImageWrapper}>
+          <Image
+            alt={alt}
+            css={styles.previewImage}
+            fill
+            src={URL.createObjectURL(image as File)}
+            onClick={onClickImage}
+          />
+        </div>
+      ) : (
+        <button css={styles.uploadImage} onClick={onClickImage}>
+          <HiOutlineUpload size={24} />
+          <span>画像をアップロード</span>
+        </button>
+      )}
+    </>
   )
 }
