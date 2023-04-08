0c29e73aa92c7b75d139b3cb5035dbb270c7522b
whooolemilk <wholemilk25@gmail.com>
2023-04-09 01:47:37 +0900

    Containerコンポーネントに背景色：なしを追加

--- front/src/styles/components/Containers/Container.styles.ts
+++ front/src/styles/components/Containers/Container.styles.ts
@@ -11,8 +11,7 @@ export const styles = {
     min-height: ${isFull ? 'calc(100vh - 56px)' : 'auto'};
     padding: 0 16px;
     text-align: ${isCenter ? 'center' : 'start'};
-    background-color: ${bgColor === 'white'
-      ? theme.color.white
-      : theme.color.bgBlue};
+    background-color: ${bgColor === 'white' && theme.color.white}${bgColor ===
+        'blue' && theme.color.bgBlue}${bgColor === 'none' && 'none'};
   `
 }
