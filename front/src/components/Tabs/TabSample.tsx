fa6000ea061cffa032f9580b15f601d32d32bf02
Miyuu <74092547+whooolemilk@users.noreply.github.com>
2023-04-18 14:47:12 +0900

    🌻 travelinkページのマークアップ (#53)
    * ButtonIconWithTextコンポーネントの作成
    
    * ImageThumbnailコンポーネントの作成
    
    * TabSampleコンポーネントの作成
    
    * TabDaysWithTravelinkコンポーネントの作成
    
    * Travelinkページのマークアップ
    
    * TabDaysWithTravelinkコンポーネントをEmotion.jsに置き換えてるけど動かんくなった件について
    
    * 適当ぷっしゅ
    
    * TabDaysWithTravelinkコンポーネントをEmotionで書き換え
    
    * ImageThumbnailコンポーネントをEmotionに書き換えとStorybookに追加
    
    * ButtonIconWithTextコンポーネントをStorybookに追加
    
    * TabDaysWithTravelinkコンポーネントのstyle修正
    
    * TabDaysWithTravelinkコンポーネント修正
    
    * TabSampleコンポーネントをStorybookに追加
    
    * 修正
    
    * NuHTMLCheckerのエラー対応
    
    * spanをブロック要素にする
    
    * key問題直す

---
+++ front/src/components/Tabs/TabSample.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Container } from '@/components/Containers'
+import { styles } from '@/styles/components/Tabs/TabSample.styles'
+
+type TabSampleProps = {
+  data: {
+    name: number
+    value: string
+  }[]
+}
+
+export const TabSample = ({ data }: TabSampleProps) => {
+  return (
+    <div css={styles.wrapper}>
+      <div css={styles.tabs} id="tabs">
+        <input
+          css={styles.tabSwitch}
+          defaultChecked
+          id="tab1"
+          name="tab"
+          type="radio"
+        />
+        <label css={styles.tabLabel} htmlFor="tab1">
+          {data[0].name}
+        </label>
+        <div css={styles.tabContent}>
+          <Container bgColor="blue">
+            {/* タブの中身を入れる */}
+            {data[0].value}
+          </Container>
+        </div>
+
+        {data.map((item) => (
+          <>
+            {item.name !== 1 && (
+              <React.Fragment key={item.name}>
+                <input
+                  css={styles.tabSwitch}
+                  id={`tab${item.name}`}
+                  name="tab"
+                  type="radio"
+                />
+                <label css={styles.tabLabel} htmlFor={`tab${item.name}`}>
+                  {item.name}
+                </label>
+                <div css={styles.tabContent}>
+                  <Container bgColor="blue">
+                    {/* タブの中身を入れる */}
+                    {item.value}
+                  </Container>
+                </div>
+              </React.Fragment>
+            )}
+          </>
+        ))}
+      </div>
+    </div>
+  )
+}
---
+++ front/src/pages/travelink/[travelinkId]/index.tsx
@@ -0,0 +1,46 @@
+import { ButtonIconWithText } from '@/components/Buttons'
+import { Container } from '@/components/Containers'
+import { ImageThumbnail } from '@/components/Images'
+import { TabDaysWithTravelink } from '@/components/Tabs'
+import { styles } from '@/styles/pages/travelink/[travelinkId]/Travelink.styles'
+
+const Travelink = () => {
+  // example
+  const data = {
+    url: '/images/thumbnail_sample.jpg',
+    travelinks: [
+      { day: 1, name: 'aaa' },
+      { day: 2, name: 'bbb' },
+      { day: 3, name: 'ccc' },
+      { day: 4, name: 'ddd' },
+      { day: 5, name: 'eee' },
+      { day: 6, name: 'fff' },
+      { day: 7, name: 'ggg' },
+      { day: 8, name: 'hhh' },
+      { day: 9, name: 'iii' },
+      { day: 10, name: 'jjj' }
+    ]
+  }
+  return (
+    <>
+      <ImageThumbnail url={data.url} />
+      {/* モーダル */}
+      <div css={styles.modalWrapper}>
+        <div css={styles.handler}></div>
+        <Container bgColor="white" isCenter>
+          <h1 css={styles.title}>田舎にゆったり旅行</h1>
+          <p css={styles.date}>2023.03.25 - 2023.03.27</p>
+          <div css={styles.layoutButtonIconWithText}>
+            <ButtonIconWithText iconType="members" />
+            <ButtonIconWithText iconType="memo" />
+            <ButtonIconWithText iconType="share" />
+            <ButtonIconWithText iconType="edit" />
+            <ButtonIconWithText iconType="public" />
+          </div>
+        </Container>
+        <TabDaysWithTravelink travelinks={data.travelinks} />
+      </div>
+    </>
+  )
+}
+export default Travelink
