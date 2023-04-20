051bf81fed685ebc954fc3be88f06a4dd06a1062
eve00 <64850924+eve00@users.noreply.github.com>
2023-04-20 22:10:44 +0900

    TavelListItem作成

--- front/src/components/travelLists/TravelListItem.tsx
+++ front/src/components/travelLists/TravelListItem.tsx
@@ -1,31 +1,37 @@
+import Image from "next/image"
 import { HiOutlineClock, HiOutlineDocumentText } from "react-icons/hi"
 import { styles } from "@/styles/components/travelLists/TravelListItem.styles"
 
-type TravelListItemProps = {
-    icon: 'map' | 'link'
-    title: string
-    date: string
-    title_memo: string
+export type TravelListItemType = {
+  icon: 'map' | 'link' | 'ledger'
+  title: string
+  date?: string
+  memo?: string
 }
 
-export const TravelListItem = ({title, date, title_memo,...props}:TravelListItemProps) => {
-    return (
-      <div css={styles.item}>
-        <div css={styles.icon}></div>
-        <div css={styles.values}>
-          <div css={styles.title}>title</div>
-          <div css={styles.sub}>
-            <div css={styles.text_with_icon}>
-              <HiOutlineClock size={16} />
-              <span>18:00</span>
-            </div>
-            <div css={styles.text_with_icon}>
-              <HiOutlineDocumentText size={16} />
-              <HiOutlineDocumentText size={16} />
-              <p>memomemo</p>
-            </div>
-          </div>
+export const TravelListItem = (item: TravelListItemType) => {
+  return (
+    <div css={styles.item}>
+      <div css={styles.icon}>
+      {item.icon == 'map' &&
+      <Image alt="img_pin" height={24} src="/linkIcon/linkIcon_pin.svg" width={24}/>}
+    {item.icon == 'link' &&
+      <Image alt="img_link" height={24}src="/linkIcon/linkIcon_link.svg" width={24} />}
+    {item.icon == 'ledger' &&
+      <Image alt="img_ledger" height={24} src="/linkIcon/linkIcon_ledger.svg" width={24}/>}      </div>
+      <div css={styles.values}>
+        <div css={styles.title}>{item.title}</div>
+        <div css={styles.sub}>
+          {item.date && <div css={styles.text_with_icon}>
+            <HiOutlineClock css={styles.icon_small} size={16} />
+            <p>{item.date}</p>
+          </div>}
+          {item.memo && <div css={styles.text_with_icon}>
+            <HiOutlineDocumentText css={styles.icon_small} size={16} />
+            <p>{item.memo}</p>
+          </div>}
         </div>
       </div>
-    )
+    </div>
+  )
 }
--- front/src/styles/components/travelLists/TravelListItem.styles.ts
+++ front/src/styles/components/travelLists/TravelListItem.styles.ts
@@ -3,24 +3,36 @@ import { theme } from "@/styles/theme"
 
 export const styles = {
     item: css`
+    display: flex;
         width: 100%;
         height: 64px;
-
+        background:${theme.color.white};
+        border-radius:100px;
+        align-items: center;
+        box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.04);
     `,
-    values:css`
+    values: css`
         color: ${theme.color.black};
     `,
-    icon:css`
+    icon: css`
+        margin-left: 24px;
+        margin-right: 28px;
     `,
-    title:css`
+    title: css`
         font-size: ${theme.fontSize.md};
         font-weight: 600;
     `,
-    sub:css`
+    sub: css`
         display:flex;
+        gap: 24px;          
+
     `,
-    text_with_icon:css`
+    text_with_icon: css`
+    display: flex;
+    align-items: center;
             font-size: ${theme.fontSize.sm};
-            gap: 18px;
-`,
+    `,
+    icon_small: css`
+            margin-right:8px;
+    `
 }
