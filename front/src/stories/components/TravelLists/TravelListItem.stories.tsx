549ae207454977f9d3698e812d64a7f33699af14
eve00 <64850924+eve00@users.noreply.github.com>
2023-04-20 22:19:40 +0900

    TravelListItemのstory

---
+++ front/src/stories/components/TravelLists/TravelListItem.stories.tsx
@@ -0,0 +1,51 @@
+import { TravelListItem } from "@/components/travelLists"
+import { Meta, StoryObj } from "@storybook/react"
+
+const meta: Meta<typeof TravelListItem> = {
+  component: TravelListItem,
+  argTypes: {
+    icon: {
+        description: 'アイコンのタイプが入ります'
+      },
+    title: {
+      description: 'タイトルが入ります'
+    },
+    date: {
+        description: '時間が入ります'
+      },
+      memo: {
+        description: 'テキストが入ります'
+      }
+  },
+  tags: ['autodocs']
+}
+
+export default meta
+
+type Story = StoryObj<typeof TravelListItem>
+
+export const Default: Story = {
+  args: {
+    icon: 'link',
+    title: 'タイトル',
+    date: '00:00',
+    memo: 'テキストが入ります'
+  },
+  parameters: {
+    viewport: {
+      defaultViewport: 'pc'
+    }
+  }
+}
+
+export const WithoutDetails: Story = {
+    args: {
+      icon: 'link',
+      title: 'タイトル',
+    },
+    parameters: {
+      viewport: {
+        defaultViewport: 'pc'
+      }
+    }
+  }
