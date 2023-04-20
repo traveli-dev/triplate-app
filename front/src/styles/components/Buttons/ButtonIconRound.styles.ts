f1c0ba953c4fb3ca304ef23e63a1402ea89b4048
ibuki <64850924+eve00@users.noreply.github.com>
2023-04-20 17:31:26 +0900

    アニメーション追加

--- front/src/styles/components/Buttons/ButtonIconRound.styles.ts
+++ front/src/styles/components/Buttons/ButtonIconRound.styles.ts
@@ -1,4 +1,5 @@
 import { theme } from "@/styles/theme";
+import { mq } from "@/styles/utils";
 import { css } from "@emotion/react";
 
 export const styles = {
@@ -6,7 +7,67 @@ export const styles = {
     width:40px;
     height:40px;
     background: ${theme.color.white};
-    border:0;
-    border-radius:100px;
+   /* 波紋の基点とするためrelativeを指定 */
+    position: relative;
+    /* はみ出す波紋を隠す */
+    overflow: hidden;
+    cursor: pointer;
+    filter: drop-shadow(0 0 0.8px rgba(101, 119, 134, 0.2));
+    border: 0;
+    border-radius: 100px;
+    box-shadow: 0 1px 3px 1px rgba(101, 119, 134, 0.25);
+    transition: transform 0.3s;
+
+    @media screen and (prefers-reduced-motion: reduce) {
+      &::after {
+        /* 波紋 */
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        content: '';
+        background: radial-gradient(circle, #fff 10%, transparent 10%) no-repeat
+          50%;
+        opacity: 0;
+        transition: none;
+        transform: scale(10, 10);
+      }
+    }
+
+    &::after {
+      /* 波紋 */
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      content: '';
+      background: radial-gradient(circle, #fff 10%, transparent 10%) no-repeat
+        50%;
+      opacity: 0;
+      transition: transform 0.3s, opacity 1s;
+      transform: scale(10, 10);
+    }
+
+    @media screen and (prefers-reduced-motion: reduce) {
+      &:active::after {
+        opacity: 0.3;
+        transition: none;
+        transform: scale(0, 0);
+      }
+    }
+
+    &:active::after {
+      opacity: 0.3;
+      transition: 0s;
+      transform: scale(0, 0);
+    }
+
+    ${mq('sm')} {
+      &:active {
+        transform: scale(0.8);
+      }
+    }
     `
 }
