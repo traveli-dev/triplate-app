f0b2d25a8235a2e488db87cf5dd5ff87e889b16f
whooolemilk <wholemilk25@gmail.com>
2023-04-25 23:55:53 +0900

    Chipコンポーネントを修正

---
+++ front/src/styles/components/Chips/ChipTagList.styles.ts
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react'
+
+export const styles = {
+  wrapper: css`
+    display: flex;
+    gap: 8px;
+    flex-wrap: wrap;
+    padding: 0;
+    list-style: none;
+  `
+}
