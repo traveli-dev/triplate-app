7a0181dfc3fea3e4ba10f90b05770cdb85796de2
whooolemilk <wholemilk25@gmail.com>
2023-04-25 19:28:55 +0900

    ModalEditコンポーネントを修正

--- front/src/components/Modals/ModalEdit.tsx
+++ front/src/components/Modals/ModalEdit.tsx
@@ -2,7 +2,7 @@ import {
   HiOutlineInformationCircle,
   HiOutlineClipboardList
 } from 'react-icons/hi'
-import { LinkItemWithIcon } from '@/components/Links'
+import { LinkIconWithTextHorizontal } from '@/components/Links'
 import { BaseHalfModal } from '@/components/Modals'
 import { styles } from '@/styles/components/Modals/ModalEdit.styles'
 
@@ -15,14 +15,14 @@ export const ModalEdit = ({ isOpen, onClose }: ModalEditProps) => {
   return (
     <BaseHalfModal isOpen={isOpen} title="編集する" onClose={onClose}>
       <div css={styles.layoutLinkItem}>
-        <LinkItemWithIcon
+        <LinkIconWithTextHorizontal
           Icon={HiOutlineInformationCircle}
           href="/"
           title="基本情報を編集"
         />
       </div>
       <div css={styles.layoutLinkItem}>
-        <LinkItemWithIcon
+        <LinkIconWithTextHorizontal
           Icon={HiOutlineClipboardList}
           href="/"
           title="旅程を編集"
