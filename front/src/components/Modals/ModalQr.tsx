f693b961d80f6141367dec8db4834c6f71781894
whooolemilk <wholemilk25@gmail.com>
2023-04-25 23:57:27 +0900

    ModalQrコンポーネントを作成

--- front/src/components/Modals/ModalQr.tsx
+++ front/src/components/Modals/ModalQr.tsx
@@ -1,7 +1,5 @@
-import { HiOutlineInformationCircle } from 'react-icons/hi'
-import { LinkIconWithTextHorizontal } from '@/components/Links'
 import { BaseHalfModal } from '@/components/Modals'
-import { styles } from '@/styles/components/Modals/ModalEdit.styles'
+import { styles } from '@/styles/components/Modals/ModalQr.styles'
 
 type ModalQrProps = {
   isOpen: boolean
@@ -12,11 +10,7 @@ export const ModalQr = ({ isOpen, onClose }: ModalQrProps) => {
   return (
     <BaseHalfModal isOpen={isOpen} title="招待用QRコード" onClose={onClose}>
       <div css={styles.layoutLinkItem}>
-        <LinkIconWithTextHorizontal
-          Icon={HiOutlineInformationCircle}
-          href="/"
-          title="基本情報を編集"
-        />
+        <p css={styles.text}>QRコードを読み取ってください</p>
       </div>
     </BaseHalfModal>
   )
