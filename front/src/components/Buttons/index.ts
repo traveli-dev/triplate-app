05b562854b40358974c933e84810a9c84d71345c
Ibuki <64850924+eve00@users.noreply.github.com>
2023-05-30 01:04:25 +0900

    ⓂButtonSmall, ButtonFollowを追加 (#63)
    * ButtonSmallを追加
    
    * story
    
    * ButtonFollowを追加
    
    * 謎差分
    
    * emotionに置き換える
    
    * d.tsファイルを削除
    
    * scss関連ファイル削除
    
    * pass帰る
    
    * 勝手にリファクタ
    
    ---------
    
    Co-authored-by: Daiki Maekawa <daaikii@outlook.jp>
    Co-authored-by: whooolemilk <wholemilk25@gmail.com>
    Co-authored-by: Miyuu <74092547+whooolemilk@users.noreply.github.com>

--- front/src/components/Buttons/index.ts
+++ front/src/components/Buttons/index.ts
@@ -1,6 +1,8 @@
 export { ButtonFill } from '@/components/Buttons/ButtonFill'
 export { ButtonOutline } from '@/components/Buttons/ButtonOutline'
 export { ButtonIconRound } from '@/components/Buttons/ButtonIconRound'
+export { ButtonSmall } from '@/components/Buttons/ButtonSmall'
+export { ButtonFollow } from '@/components/Buttons/ButtonFollow'
 export { FloatingActionButton } from '@/components/Buttons/FloatingActionButton'
 export { ButtonIconWithText } from '@/components/Buttons/ButtonIconWithText'
 export { ButtonIconWIthTextHorizontal } from '@/components/Buttons/ButtonIconWithTextHorizontal'
