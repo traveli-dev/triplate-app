e9bafb7c5c57c4975add3dcfe0ccc368957e9c37
Daiki Maekawa <daaikii@outlook.jp>
2023-06-02 20:26:43 +0900

    お前を消す

--- front/src/components/Forms/index.ts
+++ front/src/components/Forms/index.ts
@@ -1,3 +1,2 @@
 export { FormCreateUpdateTriplinks } from '@/components/Forms/FormCreateUpdateTriplinks'
 export { FormAuthority } from '@/components/Forms/FormAuthority'
-export { FormCreateUpdateUser } from '@/components/Forms/FormCreateUpdateUser'
