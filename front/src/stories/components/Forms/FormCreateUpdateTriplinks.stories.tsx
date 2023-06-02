34fd03abde65c951a3cade54656c04ec53de0713
Daiki Maekawa <daaikii@outlook.jp>
2023-06-02 20:21:59 +0900

    storybook修正

---
+++ front/src/stories/components/Forms/FormCreateUpdateTriplinks.stories.tsx
@@ -0,0 +1,13 @@
+import { FormCreateUpdateTriplinks } from '@/components/Forms'
+import type { StoryObj, Meta } from '@storybook/react'
+
+const meta: Meta<typeof FormCreateUpdateTriplinks> = {
+  component: FormCreateUpdateTriplinks,
+  tags: ['autodocs']
+}
+
+export default meta
+
+type Story = StoryObj<typeof FormCreateUpdateTriplinks>
+
+export const Default: Story = {}
