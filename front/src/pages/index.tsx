0a575602580d3c8920ae43c1db703bbdd2fbd789
Daiki Maekawa <67625825+Daaiki@users.noreply.github.com>
2023-06-07 15:24:01 +0900

    確認

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -46,7 +46,7 @@ const Index = () => {
           </ButtonFill>
           <ButtonFill onClick={signOutHandler}>サインアウト</ButtonFill>
         </div>
-        <div>CI確認用</div>
+        <div>CI確認</div>
         <div>
           <Link href="/home">/home</Link>
         </div>
