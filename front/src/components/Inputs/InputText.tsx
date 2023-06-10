1a9053c05c3adf36d1021c3bdd736edcceb0a48c
Daiki Maekawa <daaikii@outlook.jp>
2023-06-10 21:24:47 +0900

    forwardRefをつける

--- front/src/components/Inputs/InputText.tsx
+++ front/src/components/Inputs/InputText.tsx
@@ -1,17 +1,21 @@
-import { ComponentPropsWithRef } from 'react'
+import { ComponentPropsWithRef, forwardRef } from 'react'
 import { styles } from '@/styles/components/Inputs/InputText.styles'
 
 type InputTextProps = ComponentPropsWithRef<'input'> & {
   placeholder: string
 }
 
-export const InputText = ({ placeholder, ...props }: InputTextProps) => {
+export const InputText = forwardRef<HTMLInputElement, InputTextProps>(({ placeholder, ...props }, ref) => {
   return (
     <input
       css={styles.input}
       placeholder={placeholder}
+      ref={ref}
       type="text"
       {...props}
     />
   )
-}
+})
+
+// https://github.com/jsx-eslint/eslint-plugin-react/issues/2269
+InputText.displayName = "InputText"
--- front/src/components/Inputs/InputTextArea.tsx
+++ front/src/components/Inputs/InputTextArea.tsx
@@ -1,13 +1,16 @@
-import { ComponentPropsWithRef } from 'react'
+import { ComponentPropsWithRef, forwardRef } from 'react'
 import { styles } from '@/styles/components/Inputs/InputTextArea.styles'
 
 type InputTextAreaProps = ComponentPropsWithRef<'textarea'> & {
   placeholder: string
 }
 
-export const InputTextArea = ({
+export const InputTextArea = forwardRef<HTMLTextAreaElement, InputTextAreaProps>(({
   placeholder,
   ...props
-}: InputTextAreaProps) => {
-  return <textarea css={styles.textarea} placeholder={placeholder} {...props} />
-}
+}, ref) => {
+  return <textarea css={styles.textarea} placeholder={placeholder} ref={ref} {...props} />
+})
+
+// https://github.com/jsx-eslint/eslint-plugin-react/issues/2269
+InputTextArea.displayName = "InputTextArea"
