d8a33ce819ab271448e5d9d106426601769a40b5
eve00 <64850924+eve00@users.noreply.github.com>
2023-06-11 19:48:25 +0900

    exploresのデータを取得する

---
+++ front/src/redux/services/firestore/explores.ts
@@ -0,0 +1,46 @@
+import {
+    collection,
+    CollectionReference,
+    doc,
+    DocumentReference,
+    getDoc,
+    getDocs
+    // Timestamp
+} from 'firebase/firestore'
+import { db } from '@/lib/firebase'
+import { baseFirestoreApi } from '@/redux/services/firestore'
+
+export type GetExploreType = ExploreType & {
+    id: string
+}
+
+export type ExploreType = {
+    title: string
+    day: string
+    keywords: string[]
+    thumbnail: string
+}
+
+const exploresApi = baseFirestoreApi.injectEndpoints({
+    endpoints: (builder) => ({
+        getAllExplores: builder.query<ExploreType[], void>({
+            queryFn: async () => {
+                try {
+                    const snapshot = await getDocs(collection(db, 'explores'))
+                    
+                    const ret = snapshot.docs.map((doc) => {
+                        return { ...doc.data() } as ExploreType
+                    })
+                    console.log(ret);
+                    
+                    return { data: ret }
+                } catch (err) {
+                    // TODO: エラー処理
+                    return { error: err }
+                }
+            }
+        })
+    }),
+    overrideExisting: false
+})
+export const { useGetAllExploresQuery } = exploresApi
