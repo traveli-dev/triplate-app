7b570199ce43ebe1b5db6d8b5deb0794b48a173e
eve00 <64850924+eve00@users.noreply.github.com>
2023-06-16 01:01:09 +0900

    triplateページのマークアップ（タブなし）

--- front/src/pages/triplink/triplate.tsx
+++ front/src/pages/triplink/triplate.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
-import { HiDotsVertical } from 'react-icons/hi'
-import { ButtonFill } from '@/components/Buttons'
+import Link from 'next/link'
+import { HiDotsVertical, HiOutlineDuplicate, HiOutlineHeart, HiOutlinePencil, HiOutlineQuestionMarkCircle, HiOutlineTag } from 'react-icons/hi'
+import { ButtonFill, ButtonWithIcon } from '@/components/Buttons'
+import { CardTripListItem } from '@/components/Cards'
 import { Container } from '@/components/Containers'
-import { TabDaysWithTriplink } from '@/components/Tabs'
 import { TravelListItemType } from '@/components/Tabs/TabDaysWithTriplink'
-import { styles } from '@/styles/pages/triplink/triplate.styles'
+import { styles, contentStyles, CardUserProfileStyles, FillButtonStyles, OutlineButtonStyles } from '@/styles/pages/triplink/triplate.styles'
+import { Header } from '@/components/Headers'
 
 const Triplate = () => {
   const items: TravelListItemType[] = [
@@ -13,47 +15,128 @@ const Triplate = () => {
     { icon: 'ledger', title: 'タイトル３', date: '', memo: 'memomemo' },
     { icon: 'map', title: 'タイトル４', date: '', memo: '' }
   ]
-  const _triplinks = [
-    { day: 1, name: 'aaa', linkList: items },
-    { day: 2, name: 'bbb', linkList: items },
-    { day: 3, name: 'ccc', linkList: items },
-    { day: 4, name: 'ccc', linkList: items },
-    { day: 5, name: 'ccc', linkList: items }
-  ]
+  const thumbnail: string = '/images/thumbnail_sample.jpg'
+  const hashtags: string[] = ['タグ１', 'タグ２', 'タグ３', 'タグ４', 'タグ５', 'タグ６']
+
   return (
     <Container bgColor="white" isFull>
+      <Header href="/" title={"タイトル"} ToolIcon={HiOutlinePencil} toolHref='/' />
       <div css={styles.wrapper}>
-        {/*TODO: ヘッダー的な*/}
-        <div css={styles.headWrapper}>
-          <div>
-            <div css={styles.textHead}>タイトル</div>
-            <div css={styles.textSub}>サブタイトル</div>
+        <div css={styles.triplateInfosWrapper}>
+          <div css={styles.thumbnailWrapper}>
+            <Image src={thumbnail} alt={''} fill css={styles.thumbnail} />
           </div>
-          <button>
-            <HiDotsVertical size={24} />
-          </button>
-        </div>
-        <div css={styles.tab}></div>
-        <TabDaysWithTriplink tabType="view" triplinks={_triplinks} />
-      </div>
-      
-      <div css={styles.userProfileCardWrapper}>
-        <div  css={styles.userProfileWrapper}>
-          <div css={styles.userAvatarWrapper}>
-          <Image src={'/images/user_sample.jpeg'}  alt={''} fill css={styles.userAvatar}/>
-        </div>
-        <div>
-            <div css={styles.userName}>み ゆ う</div>
-            <div css={styles.userId}>@ma_ma_hima</div>
+          <div css={styles.title}>タイトル</div>
+          <div css={styles.day}>２泊３日</div>
+          <div css={styles.hashtagsWrapper}>
+            {hashtags.map((hashtag, index) => (
+              <div css={styles.hashtag} key={index}>
+                <p>#{hashtag}</p>
+              </div>
+            ))}
+          </div>
+          <div css={styles.descriptionWrapper}>
+            <p css={styles.description}>
+              DESCRIPTIONDESCRIPTION.DESCRIPTIONDESCRIPTIONDESCRIPTION.DESCRIPTION
+            </p>
           </div>
         </div>
-      <div  css={styles.userStatus}>DESCRIPTIONが入りますDESCRIPTIONが入ります</div>
-      <div css={styles.userFF}>3 フォロー | 12 フォロワー</div>
-      <div css={styles.buttonFollow}>
-        <ButtonFill>フォロー</ButtonFill>
+
+        <TabContent day={0} tripList={items} thumbnail={thumbnail} />
+        <Link css={styles.linkHelp} href="">
+          <HiOutlineQuestionMarkCircle size={20} />
+          <p>旅のテンプレートをアレンジとは？</p>
+        </Link>
+
+
+        <button css={FillButtonStyles.wrapper}>
+          <HiOutlineDuplicate size={24} />
+          <p>旅のテンプレートをアレンジする！
+          </p>        </button>
+
+
+        <button css={OutlineButtonStyles.wrapper}>
+          <HiOutlineHeart size={24} />
+          <p>
+            お気に入りに登録する
+          </p>
+        </button>
+
+
+        <CardUserProfile />
+
       </div>
-      </div>
+
+
     </Container>
   )
 }
 export default Triplate
+
+
+type TabContentType = {
+  day: number
+  thumbnail: string
+  tripList: TravelListItemType[]
+}
+const TabContent = ({ day, thumbnail, tripList }: TabContentType) => {
+  return (
+    <>
+      <Image
+        alt="thumbnail_image"
+        css={contentStyles.thumbnail}
+        height={329}
+        src={thumbnail}
+        width={329}
+      />
+      <div css={contentStyles.descriptionWrapper}>
+        <div css={contentStyles.tag}>
+          <HiOutlineTag size={20} />
+          <p>タグ</p>
+        </div>
+        <div css={contentStyles.description}>説明DESCRIPTION説明DESCRIPTION説明DESCRIPTION</div>
+        <div css={contentStyles.linkButton}>
+          <ButtonWithIcon icon="link">Google Map</ButtonWithIcon>
+          <ButtonWithIcon icon="link">Instagram</ButtonWithIcon>
+
+        </div>
+      </div>
+      <div css={contentStyles.listItemsWrapper}>
+        <div css={contentStyles.day}>Day{day}の旅程</div>
+        {tripList.map((item, _) => {
+          return (
+            <div css={contentStyles.item} key={_}>
+              <CardTripListItem
+                css={contentStyles.item}
+                date={item.date}
+                icon={item.icon}
+                memo={item.memo}
+                title={item.title}
+              />
+            </div>
+          )
+        })}
+      </div>
+
+    </>
+  )
+}
+
+const CardUserProfile = () => {
+  return (
+    <div css={CardUserProfileStyles.userProfileCardWrapper}>
+      <div css={CardUserProfileStyles.userProfileWrapper}>
+        <Image alt="" css={CardUserProfileStyles.userAvatar} height={64} src="/images/user_sample.jpeg" width={64} />
+        <div>
+          <div css={CardUserProfileStyles.userName}>み ゆ う</div>
+          <div css={CardUserProfileStyles.userId}>@ma_ma_hima</div>
+        </div>
+      </div>
+      <div css={CardUserProfileStyles.userStatus}>DESCRIPTIONが入りますDESCRIPTIONが入ります</div>
+      <div css={CardUserProfileStyles.userFF}>3 フォロー | 12 フォロワー</div>
+      <div css={CardUserProfileStyles.buttonFollow}>
+        <ButtonFill>フォロー</ButtonFill>
+      </div>
+    </div>
+  )
+}
--- front/src/styles/pages/triplink/triplate.styles.ts
+++ front/src/styles/pages/triplink/triplate.styles.ts
@@ -1,31 +1,187 @@
 import { css } from '@emotion/react'
 import { theme } from '@/styles/theme'
+import { mq } from '@/styles/utils'
 
 export const styles = {
   wrapper: css`
-    margin: 0 16px;
   `,
-  headWrapper: css`
+  
+
+  triplateInfosWrapper: css`
+  margin: 24px 28px 0 28px;
+  text-align: center;
+  `,
+  thumbnailWrapper: css`
+    width: 110px;
+  aspect-ratio: 2/1;
+    ${mq('sm')} {
+      aspect-ratio: 1/1;
+    }  
+        margin: 0 auto;
+    `,
+  thumbnail: css`
+    border-radius: 16px;
+    position: relative !important;
+    object-fit: cover;
+  `,
+  title: css`
+  margin-top: 16px;
+    font-size: ${theme.fontSize.md};
+    font-weight: 600;
+        color: ${theme.color.black};
+  `,
+  day: css`
+    margin-top: 4px;
+    color: ${theme.color.gray};
+    font-size: ${theme.fontSize.sm};  
+    
+  `,
+
+  hashtagsWrapper: css`
     display: flex;
-    justify-content: space-between;
-    padding-top: 32px;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 10px;
+    align-items: center;
+    margin-top: 4px;
   `,
-  textHead: css`
+  hashtag: css`
+    height: 29px;
+    font-size: ${theme.fontSize.sm};
+    color: ${theme.color.black};
+    background-color: ${theme.color.gray100};
+    border: 0;
+    border-radius: 100px;
+    padding: 4px 8px;
+    p {
+           line-height: 21px;
+    }
+  `,
+  descriptionWrapper: css`
+  display: block;
+      width: 241px;
+      margin: 4px auto 0 auto;
+
+  `,
+  description: css`
+    overflow-wrap: break-word;
+    color: ${theme.color.gray700};
+    font-size: ${theme.fontSize.sm};
+    
+  `,
+  linkHelp: css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: ${theme.color.black};
+    margin-top: 32px;
+    p {
+        margin-left: 16px;
+        font-size: ${theme.fontSize.sm};
+    }
+  `,
+}
+
+
+
+
+export const FillButtonStyles = {
+  wrapper: css`
+    width: 100%;
+    height: 56px;
+    color: ${theme.color.white};
+        margin-top: 16px;
+    border-radius: 16px;
+    background-color: ${theme.color.black};
+   
+      display: flex;
+    justify-content: center;
+    align-items: center;
+    p {
+        margin-left: 16px;
+        font-size: ${theme.fontSize.md};
+    }
+  `
+}
+
+
+export const OutlineButtonStyles = {
+  wrapper: css`
+    width: 100%;
+    height: 56px;
+    color: ${theme.color.black};
+    margin-top: 16px;
+    border-radius: 16px;
+    border: 2px solid ${theme.color.black};
+    background-color: ${theme.color.white};
+
+          display: flex;
+    justify-content: center;
+    align-items: center;
+    p {
+        margin-left: 10px;
+        font-size: ${theme.fontSize.md};
+    }
+ `
+}
+
+export const contentStyles = {
+  thumbnail: css`
+    position: relative !important;
+    width: 100%;
+    height: 329px;
+    margin-top: 32px;
+    border: 0;
+    border-radius: 24px;
+    object-fit: cover;
+  `,
+
+  descriptionWrapper: css`
+  padding: 16px;
+      margin-top: 16px;
+    border: 1px solid ${theme.color.outlineGray};
+    border-radius: 16px;
+  `,
+  tag: css`
+    display: flex;
+    align-items: center;
+    p { 
+      margin-left: 8px;
+      font-size: ${theme.fontSize.md};
+      font-weight: 600;
+    }
+  `,
+  description: css`
+    margin: 10px 0;
+    font-size: ${theme.fontSize.sm};
+  `,
+  linkButton: css`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 8px;
+  `,
+
+  listItemsWrapper: css`
+  padding: 16px;
+      margin-top: 40px;
+    background-color: ${theme.color.gray100};
+    border-radius: 16px;
+  `,
+  day: css`
     font-size: ${theme.fontSize.lg};
     font-weight: 600;
   `,
-  textSub: css`
-    margin-top: 8px;
-    font-size: ${theme.fontSize.md};
-  `,
+  item: css`
+    margin-top: 16px;
+  `
+}
 
-  tab: css`
-    margin-top: 40px;
-  `,
 
+export const CardUserProfileStyles = {
 
   userProfileCardWrapper: css`
     height: 266px;
+    margin-top: 32px;
   border: 1px solid ${theme.color.outlineGray};
   border-radius: 16px;
     padding: 24px 16px;
@@ -33,16 +189,11 @@ export const styles = {
   userProfileWrapper: css`
     display: flex;
   `,
-  userAvatarWrapper: css`
-    width: 64px;
-    height: 64px;
-        margin-right: 16px;
 
-  `,
   userAvatar: css`
+      margin-right: 16px;
+
     border-radius: 100px;
-    position: relative !important;
-    object-fit: cover;
   `,
 
   userName: css`
@@ -62,7 +213,6 @@ export const styles = {
   userFF: css`
         color: ${theme.color.black};
         margin-top: 8px;
-
 `,
   buttonFollow: css`
     margin-top: 24px;
