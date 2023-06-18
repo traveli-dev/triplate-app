7e499da1e385354d183e63f6fb2f2f1713eb8053
whooolemilk <wholemilk25@gmail.com>
2023-06-19 02:27:30 +0900

    ButtonFillを再調整

--- front/src/components/Buttons/ButtonFill.tsx
+++ front/src/components/Buttons/ButtonFill.tsx
@@ -1,15 +1,25 @@
 import React, { ComponentPropsWithRef } from 'react'
+import { IconType } from 'react-icons'
 import { styles } from '@/styles/components/Buttons/ButtonFill.styles'
 
 type ButtonFillProps = ComponentPropsWithRef<'button'> & {
   children: React.ReactNode
-  //TODO: icon
+  Icon?: IconType
+  isSquare?: boolean
 }
 
-export const ButtonFill = ({ children, ...props }: ButtonFillProps) => {
+export const ButtonFill = ({
+  children,
+  Icon,
+  isSquare = false,
+  ...props
+}: ButtonFillProps) => {
   return (
-    <button css={styles.button} {...props}>
-      {children}
+    <button css={styles.button(isSquare)} {...props}>
+      <div css={styles.labelWrapper}>
+        {Icon && <Icon css={styles.layoutIcon} size={24} />}
+        {children}
+      </div>
     </button>
   )
 }
--- front/src/styles/components/Buttons/ButtonFill.styles.ts
+++ front/src/styles/components/Buttons/ButtonFill.styles.ts
@@ -3,21 +3,22 @@ import { theme } from '@/styles/theme'
 import { buttonAnimation } from '@/styles/utils'
 
 export const styles = {
-  button: css`
+  button: (isSquare: boolean) => css`
     width: 100%;
-    height: 56px;
+    padding: 16px 0;
+    display: flex;
     font-size: ${theme.fontSize.md};
     font-weight: 600;
     color: ${theme.color.white};
-    background-color: ${theme.color.blue};
-    border-radius: 100px;
+    background-color: ${theme.color.black};
+    border-radius: ${isSquare ? '8px' : '100px'};
 
     &:not([disabled]) {
       ${buttonAnimation({ scale: 0.95, transition: '0.1s' })}
     }
     &:hover,
     &:focus {
-      background-color: ${theme.color.hoverBlue};
+      background-color: ${theme.color.hoverBlack};
     }
     &:disabled {
       color: ${theme.color.gray};
@@ -25,5 +26,14 @@ export const styles = {
       background-color: ${theme.color.outlineGray};
       border-color: ${theme.color.outlineGray};
     }
+  `,
+  layoutIcon: css`
+    margin-right: 10px;
+  `,
+  labelWrapper: css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
   `
 }
