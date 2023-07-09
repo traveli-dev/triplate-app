fe12648234b7e3fe6700d7ed2fbc07acf399f82b
whooolemilk <wholemilk25@gmail.com>
2023-07-09 12:34:12 +0900

    TabTriplinkコンポーネント作成

---
+++ front/src/components/Tabs/TabTriplink.tsx
@@ -0,0 +1,62 @@
+import { CardTripListItem } from '@/components/Cards'
+import { Tab, TabList, TabPanel, Tabs } from '@/components/Tabs'
+import { useTabNavigation } from '@/hooks/tabs'
+import type { TriplinkType } from '@/redux/services/firestore'
+import { styles } from '@/styles/components/Tabs/TabTriplink.styles'
+
+export type TravelListItemType = {
+  icon: 'map' | 'link' | 'ledger'
+  title: string
+  date?: string
+  memo?: string
+}
+type Itineraries = Pick<TriplinkType, 'itineraries'>
+type Triplinks = Itineraries['itineraries']
+type TabTriplinkProps = {
+  triplinks: Triplinks
+}
+
+export const TabTriplink = ({ triplinks }: TabTriplinkProps) => {
+  const tabLength = Object.keys(triplinks).length
+  const { focusedTab, tabListRef, handleTabClick, selectedTab } =
+    useTabNavigation(tabLength)
+
+  return (
+    <Tabs>
+      <TabList ref={tabListRef}>
+        {Object.keys(triplinks).map((_, index) => (
+          <Tab
+            focusedTab={focusedTab}
+            handleTabClick={handleTabClick}
+            key={index}
+            selectedTab={selectedTab}
+            tabName={`tab-${index}`}
+          >
+            {index + 1}日目
+          </Tab>
+        ))}
+      </TabList>
+      {Object.values(triplinks).map((value, index) => (
+        <TabPanel
+          key={index}
+          panelName={`day-${index + 1}`}
+          selectedTab={selectedTab}
+          tabName={`tab-${index}`}
+        >
+          {value.map((plan, index) => (
+            <div css={styles.layoutCardTriplinkListItem} key={index}>
+              <CardTripListItem
+                date={null}
+                icon="map"
+                // TODO: iconを動的に
+                memo={plan.memo}
+                title={plan.name}
+                // TODO: date={plan.time}
+              />
+            </div>
+          ))}
+        </TabPanel>
+      ))}
+    </Tabs>
+  )
+}
--- front/src/pages/triplink/[triplinkId]/index.tsx
+++ front/src/pages/triplink/[triplinkId]/index.tsx
@@ -8,10 +8,11 @@ import {
   ModalShare,
   ModalMember
 } from '@/components/Modals'
-import { TabDaysWithTriplink } from '@/components/Tabs'
+import { TabDaysWithTriplink, TabTriplink } from '@/components/Tabs'
 import { TravelListItemType } from '@/components/Tabs'
 import { useDisclosure } from '@/hooks/modals'
 import { styles } from '@/styles/pages/triplink/[triplinkId]/triplink.styles'
+import { TriplinkType } from '@/redux/services/firestore'
 
 const Triplink = () => {
   const router = useRouter()
@@ -24,15 +25,355 @@ const Triplink = () => {
     { icon: 'ledger', title: 'タイトル３', date: '', memo: 'memomemo' },
     { icon: 'map', title: 'タイトル４', date: '', memo: '' }
   ]
-  const data = {
-    url: '/images/thumbnail_sample.jpg',
-    triplinks: [
-      { day: 1, name: 'aaa', linkList: items },
-      { day: 2, name: 'bbb', linkList: items },
-      { day: 3, name: 'ccc', linkList: items },
-      { day: 4, name: 'ddd', linkList: items },
-      { day: 5, name: 'eee', linkList: items }
-    ]
+  type Itineraries = Pick<TriplinkType, 'itineraries'>
+
+  const data: Itineraries = {
+    itineraries: {
+      day1: [
+        {
+          id: 0,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=13121875705491233800',
+          name: '京都駅',
+          time: {
+            _seconds: 1698195600,
+            _nanoseconds: 0
+          },
+          memo: '駅に着いたら荷物預ける'
+        },
+        {
+          id: 1,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=6059551674669155929',
+          name: '京都水族館',
+          time: null,
+          memo: null
+        },
+        {
+          id: 2,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=14940864174397642651',
+          name: 'アパホテル〈京都駅前〉',
+          time: {
+            _seconds: 1698224400,
+            _nanoseconds: 0
+          },
+          memo: '荷物の回収忘れない'
+        }
+      ],
+      day2: [
+        {
+          id: 0,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=14940864174397642651',
+          name: 'アパホテル〈京都駅前〉',
+          time: {
+            _seconds: 1698285600,
+            _nanoseconds: 0
+          },
+          memo: null
+        },
+        {
+          id: 1,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=13159134450370243740',
+          name: '錦市場',
+          time: null,
+          memo: null
+        },
+        {
+          id: 2,
+          isSecret: false,
+          url: null,
+          name: 'ホテルで夕食',
+          time: {
+            _seconds: 1698314400,
+            _nanoseconds: 0
+          },
+          memo: null
+        }
+      ],
+      day3: [
+        {
+          id: 0,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=14940864174397642651',
+          name: 'ぱんだの散歩',
+          time: {
+            _seconds: 1698372000,
+            _nanoseconds: 0
+          },
+          memo: null
+        },
+        {
+          id: 1,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=5767768846672272791',
+          name: 'ぱんだの散歩',
+          time: null,
+          memo: 'みたらし団子'
+        },
+        {
+          id: 2,
+          isSecret: false,
+          url: null,
+          name: '新幹線乗って帰る',
+          time: {
+            _seconds: 1698404400,
+            _nanoseconds: 0
+          },
+          memo: null
+        },
+        {
+          id: 3,
+          isSecret: true,
+          url: 'http://rps.ana.co.jp/web/ncnv/uu/asm/all3216.php',
+          name: '新幹線webチケット',
+          time: null,
+          memo: null
+        }
+      ],
+      day4: [
+        {
+          id: 0,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=14940864174397642651',
+          name: 'ぱんだの散歩',
+          time: {
+            _seconds: 1698372000,
+            _nanoseconds: 0
+          },
+          memo: null
+        },
+        {
+          id: 1,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=5767768846672272791',
+          name: 'ぱんだの散歩',
+          time: null,
+          memo: 'みたらし団子'
+        },
+        {
+          id: 2,
+          isSecret: false,
+          url: null,
+          name: '新幹線乗って帰る',
+          time: {
+            _seconds: 1698404400,
+            _nanoseconds: 0
+          },
+          memo: null
+        },
+        {
+          id: 3,
+          isSecret: true,
+          url: 'http://rps.ana.co.jp/web/ncnv/uu/asm/all3216.php',
+          name: '新幹線webチケット',
+          time: null,
+          memo: null
+        }
+      ],
+      day5: [
+        {
+          id: 0,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=14940864174397642651',
+          name: 'ぱんだの散歩',
+          time: {
+            _seconds: 1698372000,
+            _nanoseconds: 0
+          },
+          memo: null
+        },
+        {
+          id: 1,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=5767768846672272791',
+          name: 'ぱんだの散歩',
+          time: null,
+          memo: 'みたらし団子'
+        },
+        {
+          id: 2,
+          isSecret: false,
+          url: null,
+          name: '新幹線乗って帰る',
+          time: {
+            _seconds: 1698404400,
+            _nanoseconds: 0
+          },
+          memo: null
+        },
+        {
+          id: 3,
+          isSecret: true,
+          url: 'http://rps.ana.co.jp/web/ncnv/uu/asm/all3216.php',
+          name: '新幹線webチケット',
+          time: null,
+          memo: null
+        }
+      ],
+      day6: [
+        {
+          id: 0,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=14940864174397642651',
+          name: 'ぱんだの散歩',
+          time: {
+            _seconds: 1698372000,
+            _nanoseconds: 0
+          },
+          memo: null
+        },
+        {
+          id: 1,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=5767768846672272791',
+          name: 'ぱんだの散歩',
+          time: null,
+          memo: 'みたらし団子'
+        },
+        {
+          id: 2,
+          isSecret: false,
+          url: null,
+          name: '新幹線乗って帰る',
+          time: {
+            _seconds: 1698404400,
+            _nanoseconds: 0
+          },
+          memo: null
+        },
+        {
+          id: 3,
+          isSecret: true,
+          url: 'http://rps.ana.co.jp/web/ncnv/uu/asm/all3216.php',
+          name: '新幹線webチケット',
+          time: null,
+          memo: null
+        }
+      ],
+      day7: [
+        {
+          id: 0,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=14940864174397642651',
+          name: 'ぱんだの散歩',
+          time: {
+            _seconds: 1698372000,
+            _nanoseconds: 0
+          },
+          memo: null
+        },
+        {
+          id: 1,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=5767768846672272791',
+          name: 'ぱんだの散歩',
+          time: null,
+          memo: 'みたらし団子'
+        },
+        {
+          id: 2,
+          isSecret: false,
+          url: null,
+          name: '新幹線乗って帰る',
+          time: {
+            _seconds: 1698404400,
+            _nanoseconds: 0
+          },
+          memo: null
+        },
+        {
+          id: 3,
+          isSecret: true,
+          url: 'http://rps.ana.co.jp/web/ncnv/uu/asm/all3216.php',
+          name: '新幹線webチケット',
+          time: null,
+          memo: null
+        }
+      ],
+      day8: [
+        {
+          id: 0,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=14940864174397642651',
+          name: 'ぱんだの散歩',
+          time: {
+            _seconds: 1698372000,
+            _nanoseconds: 0
+          },
+          memo: null
+        },
+        {
+          id: 1,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=5767768846672272791',
+          name: 'ぱんだの散歩',
+          time: null,
+          memo: 'みたらし団子'
+        },
+        {
+          id: 2,
+          isSecret: false,
+          url: null,
+          name: '新幹線乗って帰る',
+          time: {
+            _seconds: 1698404400,
+            _nanoseconds: 0
+          },
+          memo: null
+        },
+        {
+          id: 3,
+          isSecret: true,
+          url: 'http://rps.ana.co.jp/web/ncnv/uu/asm/all3216.php',
+          name: '新幹線webチケット',
+          time: null,
+          memo: null
+        }
+      ],
+      day9: [
+        {
+          id: 0,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=14940864174397642651',
+          name: 'ぱんだの散歩',
+          time: {
+            _seconds: 1698372000,
+            _nanoseconds: 0
+          },
+          memo: null
+        },
+        {
+          id: 1,
+          isSecret: false,
+          url: 'https://maps.google.com/?cid=5767768846672272791',
+          name: 'ぱんだの散歩',
+          time: null,
+          memo: 'みたらし団子'
+        },
+        {
+          id: 2,
+          isSecret: false,
+          url: null,
+          name: '新幹線乗って帰る',
+          time: {
+            _seconds: 1698404400,
+            _nanoseconds: 0
+          },
+          memo: null
+        },
+        {
+          id: 3,
+          isSecret: true,
+          url: 'http://rps.ana.co.jp/web/ncnv/uu/asm/all3216.php',
+          name: '新幹線webチケット',
+          time: null,
+          memo: null
+        }
+      ]
+    }
   }
 
   const {
@@ -62,8 +403,9 @@ const Triplink = () => {
   return (
     <>
       <ButtonIconRound icon="back" onClick={() => router.push('/home')} />
-      <ImageThumbnail url={data.url} />
+      <ImageThumbnail url="/images/thumbnail_sample.jpg" />
       {/* モーダル */}
+      {/* TODO: 一旦モーダルやめて、スクロールしたらタブが上に止まるように */}
       <div css={styles.modalWrapper}>
         <div css={styles.handler}></div>
         <Container bgColor="white" isCenter>
@@ -84,7 +426,7 @@ const Triplink = () => {
           onClose={memoOnClose}
         />
         <ModalShare isOpen={shareIsOpen} onClose={shareOnClose} />
-        <TabDaysWithTriplink triplinks={data.triplinks} />
+        <TabTriplink triplinks={data.itineraries} />
       </div>
     </>
   )
