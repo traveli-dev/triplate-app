25c7f4a1cc0a785d5ed5b7ee3c009d5c621c3fde
whooolemilk <wholemilk25@gmail.com>
2023-07-09 18:28:14 +0900

    settingsページ完成

--- front/src/pages/settings/index.tsx
+++ front/src/pages/settings/index.tsx
@@ -1,32 +1,114 @@
-import Link from 'next/link'
+import {
+  HiOutlineChevronRight,
+  HiOutlineUser,
+  HiOutlineBell,
+  HiOutlineLocationMarker,
+  HiOutlineMail,
+  HiOutlineShieldCheck,
+  HiOutlineQrcode,
+  HiOutlineQuestionMarkCircle,
+  HiOutlinePencil
+} from 'react-icons/hi'
+import { Container } from '@/components/Containers'
 import { Header } from '@/components/Headers'
+import { List, ListItem } from '@/components/Lists'
+import { styles } from '@/styles/pages/settings/index.styles'
 
 const Settings = () => {
   const userId = 'ma_ma_hima'
 
+  const mainSettings = [
+    {
+      title: 'プロフィール設定',
+      path: '/settings/profile',
+      icon: HiOutlineUser
+    },
+    {
+      title: '通知設定',
+      path: '/settings/notifications',
+      icon: HiOutlineBell
+    },
+    {
+      title: '位置情報設定',
+      path: '/settings/location',
+      icon: HiOutlineLocationMarker
+    }
+  ]
+
+  const tripSettings = [
+    {
+      title: 'たびのテンプレートを管理',
+      path: '/dashboard',
+      icon: HiOutlinePencil
+    },
+    {
+      title: 'QRコードリーダー',
+      path: '/reader',
+      icon: HiOutlineQrcode
+    }
+  ]
+
+  const otherSettings = [
+    {
+      title: 'お問い合わせ',
+      path: '/contact',
+      icon: HiOutlineMail
+    },
+    {
+      title: 'ヘルプ',
+      path: '/help',
+      icon: HiOutlineQuestionMarkCircle
+    },
+    {
+      title: 'プライバシーポリシー',
+      path: '/privacy',
+      icon: HiOutlineShieldCheck
+    }
+  ]
+
   return (
     <>
       <Header href={`/${userId}`} title="設定" />
-      <h2>全般</h2>
-      <div>
-        <Link href="/settings/profile">プロフィール設定</Link>
-      </div>
-      <div>
-        <Link href="/settings/notifications">通知設定</Link>
-      </div>
-      <div>
-        <Link href="/settings/location">位置情報設定</Link>
-      </div>
-      <h2>その他</h2>
-      <div>
-        <Link href="/support">お問い合わせ</Link>
-      </div>
-      <div>
-        <Link href="/help">ヘルプ</Link>
-      </div>
-      <div>
-        <Link href="/privacy">プライバシーポリシー</Link>
-      </div>
+      <Container isFull bgColor="white" isPaddingNone>
+        <h2 css={styles.header}>全般</h2>
+        <List border>
+          {mainSettings.map((setting, index) => (
+            <ListItem
+              Icon={setting.icon}
+              IconRight={HiOutlineChevronRight}
+              href={setting.path}
+              key={index}
+              title={setting.title}
+            />
+          ))}
+        </List>
+
+        <h2 css={styles.header}>たび関連</h2>
+        <List border>
+          {tripSettings.map((setting, index) => (
+            <ListItem
+              Icon={setting.icon}
+              IconRight={HiOutlineChevronRight}
+              href={setting.path}
+              key={index}
+              title={setting.title}
+            />
+          ))}
+        </List>
+
+        <h2 css={styles.header}>その他</h2>
+        <List border>
+          {otherSettings.map((setting, index) => (
+            <ListItem
+              Icon={setting.icon}
+              IconRight={HiOutlineChevronRight}
+              href={setting.path}
+              key={index}
+              title={setting.title}
+            />
+          ))}
+        </List>
+      </Container>
     </>
   )
 }
---
+++ front/src/styles/pages/settings/index.styles.ts
@@ -0,0 +1,9 @@
+import { css } from '@emotion/react'
+import { theme } from '@/styles/theme'
+
+export const styles = {
+  header: css`
+    font-size: ${theme.fontSize.md};
+    margin: 16px 16px 8px 16px;
+  `
+}
