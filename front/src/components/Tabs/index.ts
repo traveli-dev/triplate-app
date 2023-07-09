865ec102366bc4ae1184e38e9322338ae6a034b4
whooolemilk <wholemilk25@gmail.com>
2023-07-09 13:22:22 +0900

    不必要なタブコンポーネント削除

--- front/src/components/Tabs/index.ts
+++ front/src/components/Tabs/index.ts
@@ -1,5 +1,4 @@
 export { TabHome } from '@/components/Tabs/TabHome'
-export { TabContent } from '@/components/Tabs/TabContent'
 export { Tab } from '@/components/Tabs/Tab'
 export { Tabs } from '@/components/Tabs/Tabs'
 export { TabPanel } from '@/components/Tabs/TabPanel'
