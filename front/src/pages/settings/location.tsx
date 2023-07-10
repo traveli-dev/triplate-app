1176ac5894dcd677908783e3a739a830f501327d
whooolemilk <wholemilk25@gmail.com>
2023-07-11 01:35:49 +0900

    registerなしのInputSwitchをコメントアウト

--- front/src/pages/settings/location.tsx
+++ front/src/pages/settings/location.tsx
@@ -4,13 +4,14 @@ import { InputSwitch } from '@/components/Inputs'
 import { styles } from '@/styles/pages/settings/settingsLocation.styles'
 
 const SettingsLocation = () => {
+  // TODO: 以下、Inputにregisterを追加する
   return (
     <>
       <Header href="/settings" title="位置情報設定" />
       <Container bgColor="white" isFull>
         <h2 css={styles.headerMail}>位置情報</h2>
         <div css={styles.layoutInput}>
-          <InputSwitch id="mail-news" text="位置情報の取得を許可する" />
+          {/* <InputSwitch id="mail-news" text="位置情報の取得を許可する" /> */}
         </div>
       </Container>
     </>
--- front/src/pages/settings/notifications.tsx
+++ front/src/pages/settings/notifications.tsx
@@ -4,23 +4,24 @@ import { InputSwitch } from '@/components/Inputs/InputSwitch'
 import { styles } from '@/styles/pages/settings/notifications.styles'
 
 const Notifications = () => {
+  // TODO: 以下、Inputにregisterを追加する
   return (
     <>
       <Header href="/settings" title="通知設定" />
       <Container bgColor="white" isFull>
         <h2 css={styles.headerMail}>メール通知</h2>
         <div css={styles.layoutInput}>
-          <InputSwitch id="mail-news" text="公式からのお知らせ" />
+          {/* <InputSwitch id="mail-news" text="公式からのお知らせ" /> */}
         </div>
         <div css={styles.layoutInput}>
-          <InputSwitch id="mail-follow" text="フォロー通知" />
+          {/* <InputSwitch id="mail-follow" text="フォロー通知" /> */}
         </div>
         <h2 css={styles.headerPush}>プッシュ通知</h2>
         <div css={styles.layoutInput}>
-          <InputSwitch id="push-news" text="公式からのお知らせ" />
+          {/* <InputSwitch id="push-news" text="公式からのお知らせ" /> */}
         </div>
         <div css={styles.layoutInput}>
-          <InputSwitch id="push-follow" text="フォロー通知" />
+          {/* <InputSwitch id="push-follow" text="フォロー通知" /> */}
         </div>
       </Container>
     </>
