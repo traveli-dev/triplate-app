60a4f5920d7973f80d965466e88e0c8b8571470b
eve00 <64850924+eve00@users.noreply.github.com>
2023-07-11 22:25:01 +0900

    CardUserProfileにユーザデータを流し込む（FFデータ以外）

--- front/src/components/Cards/CardUserProfile.tsx
+++ front/src/components/Cards/CardUserProfile.tsx
@@ -1,8 +1,13 @@
 import Image from 'next/image'
 import { ButtonFill } from '@/components/Buttons'
 import { styles } from '@/styles/components/Cards/CardUserProfile.styles'
+import { UserType } from '@/redux/services/firestore'
 
-export const CardUserProfile = () => {
+type CardUserProfileProps = {
+  userData: UserType
+}
+
+export const CardUserProfile = ({userData }: CardUserProfileProps) => {
   return (
     <div css={styles.userProfileCardWrapper}>
       <div css={styles.userProfileWrapper}>
@@ -10,18 +15,20 @@ export const CardUserProfile = () => {
           alt=""
           css={styles.userAvatar}
           height={64}
-          src="/images/user_sample.jpeg"
+          src={userData.icon}
           width={64}
         />
         <div>
-          <div css={styles.userName}>み ゆ う</div>
-          <div css={styles.userId}>@ma_ma_hima</div>
+          <div css={styles.userName}>{userData.name}</div>
+          <div css={styles.userId}>@{userData.userId}</div>
         </div>
       </div>
       <div css={styles.userStatus}>
-        DESCRIPTIONが入りますDESCRIPTIONが入ります
+        {userData.description}
       </div>
+      {/* TODO:FFデータ（別タスクにて修正） */}
       <div css={styles.userFF}>3 フォロー | 12 フォロワー</div>
+      {/* TODO:フォロー */}
       <div css={styles.buttonFollow}>
         <ButtonFill>フォロー</ButtonFill>
       </div>
